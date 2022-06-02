c2366e1ccbc58e40dae1a00e34c8625cfc2e1bc0
jschoonveld <jesse.schoonveld@gmail.com>
2022-06-02 20:09:59 +0200

    initial commit

---
+++ client/components/TaskSection.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="task-section-container">
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .task-section-container {
+    .block {
+      height: 50px;
+      width: 50px;
+      background-color: red;
+    }
+  }
+</style>
+
+<script>
+export default {
+  name: 'TaskSection'
+}
+</script>
---
+++ client/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <TaskSection />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
